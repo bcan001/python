1358ce7aaa38494ec060c6034a23994a8a3691b4
Ben Caneba <bcaneba@gmail.com>
2015-06-15 14:55:05 -0700

    inheritance example

--- guessGame.py
+++ guessGame.py
@@ -1,24 +1,57 @@
 # guessing game using python
 
-class GuessingGame:
-  def __init__(self, game_number):
-    if isinstance(game_number,int):
-      self.game_number = game_number
-      self.user_guess = 0
-      self.done = False
-    else:
-      self.done = True
+# class GuessingGame:
+#   def __init__(self, game_number):
+#     if isinstance(game_number,int):
+#       self.game_number = game_number
+#       self.user_guess = 0
+#       self.done = False
+#     else:
+#       self.done = True
 
+#   def play(self):
+#     while (self.done == False):
+#       self.get_user_guess()
+#       self.check_guess()
+#     print "You are correct!"
+
+#   def get_user_guess(self):
+#     self.user_guess = raw_input("What number would you like to Guess?")
+#     self.user_guess = int(self.user_guess)
+#     return self.user_guess
+
+#   def check_guess(self):
+#     # self.user_guess is always greater than self.game_number
+#     if self.user_guess > self.game_number:
+#       print "Too High"
+#     elif self.user_guess < self.game_number:
+#       print "Too Low"
+#     else:
+#       self.done = True
+
+# game = GuessingGame(45)
+# # print game.game_number
+# # print game.user_guess
+# # print game.done
+
+# # print game.get_user_guess()
+# game.play()
+
+
+# refactored python code:
+import random
+
+class GuessingGame:
+  def __init__(self):
+    self.game_number = random.randint(0,50)
+    self.user_guess = 0
+    self.done = False
+    
   def play(self):
     while (self.done == False):
       self.get_user_guess()
       self.check_guess()
     print "You are correct!"
-
-  def get_user_guess(self):
-    self.user_guess = raw_input("What number would you like to Guess?")
-    self.user_guess = int(self.user_guess)
-    return self.user_guess
 
   def check_guess(self):
     # self.user_guess is always greater than self.game_number
@@ -29,13 +62,22 @@ class GuessingGame:
     else:
       self.done = True
 
-game = GuessingGame(45)
+  def get_user_guess(self):
+    self.user_guess = raw_input("What number would you like to Guess? (0-50)")
+    self.user_guess = int(self.user_guess)
+    return self.user_guess
+
+game = GuessingGame()
 # print game.game_number
 # print game.user_guess
 # print game.done
 
 # print game.get_user_guess()
 game.play()
+
+
+
+
 
 # below is ruby code for the same game --------------------------------------------------
 
