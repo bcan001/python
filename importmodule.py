a239bb59f1a761d61ba5a8a3f341c2706d845a35
Ben Caneba <bcaneba@gmail.com>
2015-06-16 10:55:44 -0700

    added inheritance

---
+++ importmodule.py
@@ -0,0 +1,10 @@
+import inheritance
+
+cat = inheritance.Cat("Mister", "Cat")
+print cat.hatesDogs() 				# from Cat class 
+print cat.getSpecies()        # inherited from Pet class
+print cat.getName()						# inherited from Pet class
+print cat.__str__() 					# inherited from Pet class
+
+
+
