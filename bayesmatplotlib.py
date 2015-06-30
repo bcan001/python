f0956f046c88630093e1c23160ac63802f021858
Ben Caneba <bcaneba@gmail.com>
2015-06-29 20:27:48 -0700

    central limit thrm:

--- bayesmatplotlib.py
+++ bayesmatplotlib.py
@@ -1,24 +1,15 @@
 import numpy as np  
 import matplotlib.pyplot as plt  
 
-# def graph(formula, x_range):  
-#     x = np.array(x_range)  
-#     y = formula(x)  # <- note now we're calling the function 'formula' with x
-#     plt.plot(x, y)  
-#     plt.show()  
+def graph(formula, x_range):  
+    x = np.array(x_range)  
+    y = formula(x)  # <- note now we're calling the function 'formula' with x
+    plt.plot(x, y)  
+    plt.show()  
+def my_formula(x):
+    return x
 
-# def my_formula(x):
-#     return x**3+2*x-4
-
-# graph(my_formula, range(-10, 11))
-
-
-# =============
+graph(my_formula, range(0, 100))
 
 
-# A sampling distribution is the probability of seeing our data (X) given our parameters (θ). This is written as p(X|θ).
-# 2 means 2 possible answers, heads or tails
-# coin flipped 1000 times
-# calculates the mean distribution between heads and tails
-data_coin_flips = np.random.randint(2, size=1000)
-print np.mean(data_coin_flips)
+
--- matplotlibtest.py
+++ matplotlibtest.py
@@ -5,10 +5,22 @@ import matplotlib.pyplot as plt
 # plt.ylabel('some numbers')
 # plt.show()
 
+
  # plt.axis[xmin, xmax, ymin, ymax]
-plt.plot([1,2,3,5], [1,4,9,16], 'ro')
-plt.axis([0, 6, 0, 20])
+# plt.plot([1,2,3,5], [1,4,9,16], 'ro')
+# plt.axis([0, 6, 0, 20])
+# plt.show()
+
+
+
+# all sequences are converted to numpy arrays internally. The example below illustrates a plotting several lines with different format styles in one command using arrays.
+import numpy as np
+# evenly sampled time at 200ms intervals
+# (start of the interval, end of the interval, spacing between values)
+t = np.arange(0., 5., 0.2)
+
+# red dashes, blue squares and green triangles
+# [trangle, equation, shape]
+plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
 plt.show()
-
-
 
